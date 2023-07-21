63c6b8e131b5d47ab598f3bb17135dd8aac72d68
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-21 11:26:56 +0700

    add container not found

---
+++ src/container/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NotFoundCom from "../component/NotFoundCom";
+
+const NotFound = () => {
+
+    return (
+       <NotFoundCom />
+    );
+};
+
+export default NotFound;
