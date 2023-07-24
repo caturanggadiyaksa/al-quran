1e2b6ef962910bcc117249d14a48f59e8b3cfd44
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-24 10:43:15 +0700

    add surah number

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -8,22 +8,29 @@ const Card = ({ judul, arti, asma, surahNumber }) => {
         <div className="w-full mx-auto mt-3">
           <div className="relative group">
             <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
-            <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
-            <div className="flex items-center">
-                { asma }
-            </div>
-              <div className="space-y-2">
-                <p className="text-slate-800">
-                  {judul}
-                </p>
-                <p
-                  
-                  className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
-                >
-                  {arti}
-                </p>
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                <div className='flex items-center'>
+                <div class="flex items-center justify-center w-8 h-8 rounded-full bg-violet-500 text-white" alt="Avatar">
+                  <p>{ surahNumber }</p>
+                </div>
+
+
+                </div>
+                <div className="flex items-center ">
+                    { asma }
+                </div>
+                <div className="space-y-2">
+                    <p className="text-slate-800">
+                      {judul}
+                    </p>
+                    <p
+                      
+                      className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
+                    >
+                      {arti}
+                    </p>
+                </div>
               </div>
-            </div>
           </div>
         </div>
       </Link>
