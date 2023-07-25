c5ba33c0376e9bc8053869714e7df9da8fbf92a2
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-25 18:31:18 +0700

    deleted:    src/component/ayat/useIdsurat.jsx 	deleted:    src/component/useIdsurat.js

--- src/component/ayat/useIdsurat.jsx
+++
@@ -1,9 +0,0 @@
-// useIdsurat.jsx
-import { useParams } from 'react-router-dom';
-
-const useIdsurat = () => {
-  const { surah_number } = useParams();
-  return surah_number;
-};
-
-export default useIdsurat;
