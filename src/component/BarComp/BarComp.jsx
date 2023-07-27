c94bd177f4944718c8cac80891f85a9d46c31223
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-27 14:02:12 +0700

    add bar component

---
+++ src/component/BarComp/BarComp.jsx
@@ -0,0 +1,44 @@
+
+const BarComp = () => {
+    return (
+        <div className="pt-[6rem]">
+            <div className="mt-10 mb-4 flex justify-center items-center">
+            
+                <div className="flex flex-wrap justify-center items-center  w-2/5 py-6 rounded-full bg-slate-900 text-white text-base sm:py-2">
+                    <div className="w-[10rem]">
+                        <a
+                        className="flex flex-wrap justify-center items-center"
+                        href="website.html"
+                        >
+                       
+                        <span className="hidden md:block">Surah</span>
+                        </a>
+                    </div>
+                    <div className="w-[10rem]">
+                        <a
+                        className="flex flex-wrap justify-center items-center"
+                        href="pencapaian.html"
+                        >
+                        
+                        <span className="hidden md:block">Juz</span>
+                        </a>
+                    </div>
+                    <div className="w-[10rem]">
+                        <a
+                        className="flex flex-wrap justify-center items-center"
+                        href="portfolio.html"
+                        >
+                      
+                        <span className="hidden md:block">Bookmark</span>
+                        </a>
+                    </div>
+              
+                </div>
+            
+            </div>
+        </div>
+
+    )
+}
+
+export default BarComp;
