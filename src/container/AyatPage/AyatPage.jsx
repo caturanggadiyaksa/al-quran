e81dbe411eba82313ddf7ee3ac1b0e0dcad80816
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-28 11:00:46 +0700

    add data not availblle

--- src/container/AyatPage/AyatPage.jsx
+++ src/container/AyatPage/AyatPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Ayat from '../../component/ayat/ayat';
 import { useParams } from 'react-router-dom';
 import NotFound from '../NotFound';
+import LoaderComp from '../../component/LoaderComp/LoaderComp';
 
 const AyatPage = () => {
   const { surah_number } = useParams();
@@ -15,10 +16,9 @@ const AyatPage = () => {
     if (storedQuranData) {
       // If data exists, parse and use it
       setSurah(JSON.parse(storedQuranData));
-      setLoading(false);
-    } else {
-      setLoading(false); // No data in local storage, set loading to false
     }
+
+    setLoading(false); // Always set loading to false after attempting to fetch data
   }, []);
 
   // Tambahkan pengecekan apakah `surah_number` adalah angka
@@ -33,21 +33,27 @@ const AyatPage = () => {
       <div className='min-h-screen lg:pl-[12rem] xl:pl-[16rem] overflow-hidden bg-gray-50 dark:bg-gray-800'>
         <div className='m-4 pt-[6rem]'>
           {loading ? (
-            <p>Loading...</p>
-          ) : (
+            <LoaderComp />
+          ) : surah.length > 0 ? (
             surah.map((surahItem) => (
               <div key={surahItem.id}>
-                <h2 className='m-4 text-xl font-semibold text-gray-800 dark:text-gray-100'>{surahItem.id === parseInt(surah_number) ? surahItem.name : null}</h2>
-                {surahItem.id === parseInt(surah_number) ? surahItem.ayats.map((ayat) => (
-                  <Ayat key={ayat.id} judul={ayat.isi_ayat} arti={ayat.translation} asma={ayat.text} />
-                )) : null}
+                <h2 className='m-4 text-xl font-semibold text-gray-800 dark:text-gray-100'>
+                  {surahItem.id === parseInt(surah_number) ? surahItem.name : null}
+                </h2>
+                {surahItem.id === parseInt(surah_number)
+                  ? surahItem.ayats.map((ayat) => (
+                      <Ayat key={ayat.id} judul={ayat.isi_ayat} arti={ayat.translation} asma={ayat.text} />
+                    ))
+                  : null}
               </div>
             ))
+          ) : (
+            <div>Data not available.</div>
           )}
         </div>
       </div>
     </>
   );
-}
+};
 
 export default AyatPage;
