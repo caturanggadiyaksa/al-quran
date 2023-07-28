ef1ed97db00dd9ed98a78988e25c556a948216e4
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-07-28 11:01:21 +0700

    edit loader component

--- src/component/LoaderComp/LoaderComp.jsx
+++ src/component/LoaderComp/LoaderComp.jsx
@@ -1,11 +1,9 @@
-
+import React from "react";
 
 const LoaderComp = () => {
     return (
-        <div className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-primary opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]" role="status">
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                Loading...
-            </span>
+        <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-blue-500 border-solid"></div>
         </div>
 
     )
