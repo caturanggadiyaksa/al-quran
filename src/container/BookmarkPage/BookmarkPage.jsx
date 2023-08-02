32704a1b4faebf440ea0d09d37341a8aad490c0b
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-02 13:02:04 +0700

    use bookmark component

--- src/container/BookmarkPage/BookmarkPage.jsx
+++ src/container/BookmarkPage/BookmarkPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import BarComp from '../../component/BarComp/BarComp';
+import BookmarkComp from "../../component/BookmarkComp/BookmarkComp";
 
 const BookmarkPage = () => {
 
     return (
         <>
             <BarComp surah='/' juz='/juz' bookmark='/bookmark' />
+
+
+            <div>
+                <BookmarkComp />
+            </div>
         </>
     )
 }
