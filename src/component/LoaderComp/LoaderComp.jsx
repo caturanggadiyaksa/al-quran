ce068b34ea58bed87f4dacd80482d14cc9ad6bb7
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-09 21:09:51 +0700

    add class w-full pada parent

--- src/component/LoaderComp/LoaderComp.jsx
+++ src/component/LoaderComp/LoaderComp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoaderComp = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="w-full flex justify-center items-center h-screen">
             <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-blue-500 border-solid"></div>
         </div>
 
