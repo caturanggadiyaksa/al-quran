9ae4f430fabbd39562e3a989eccc129d02514ade
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-09 21:11:49 +0700

    refactor code akses data local storage

--- src/component/JuzComp/JuzComp.jsx
+++ src/component/JuzComp/JuzComp.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import DatajuzComp from './DatajuzComp';
+import { Link } from 'react-router-dom';
 
-// import { Link } from 'react-router-dom';
+const JuzComp = () => {
+    const collectedData = JSON.parse(localStorage.getItem('collectedData'));
 
-const JuzComp = ({ judul, arti, asma, surahNumber, totalAyat, no_juz, verse_maping }) => {
+    // Mengelompokkan data berdasarkan nomor juz
+    const groupedData = {};
+    collectedData.forEach((item) => {
+        if (!groupedData[item.juzNumber]) {
+            groupedData[item.juzNumber] = [];
+        }
+        groupedData[item.juzNumber].push(item);
+    });
 
     return (
-        <>
-       
-            <div className="bg-white p-4 rounded-lg border hover:border-blue-500 transition">
-                <h2 className="text-xl font-semibold mb-2">Juz <span>{ no_juz}</span> </h2>
-                <DatajuzComp judul={judul} arti={arti} asma={asma} surahNumber={surahNumber} totalAyat={totalAyat} verse_maping={verse_maping} />
-            </div>
-               
-
-       
-           
-
-        </>
-    )
-}
+        <div className='flex flex-wrap'>
+            {Object.keys(groupedData).map((juzNumber) => (
+                <div key={juzNumber} className='w-[33%] p-4'>
+                    <div className="min-h-[240px] bg-white rounded-lg border border-gray-300 hover:border-blue-500 transition">
+                        <div className='flex justify-between mb-2 mx-6 p-2 items-center'>
+                            <h2 className="text-xl font-semibold ">Juz <span>{juzNumber}</span></h2>
+                            <Link to={`/juz/${juzNumber}`} >
+                                <p>Baca Juz</p>
+                            </Link>
+                            
+                        </div>
+                        
+                        {groupedData[juzNumber].map((item, index) => (
+                            <DatajuzComp
+                                key={index}
+                                judul={item.judul}
+                                arti={item.arti}
+                                asma={item.asma}
+                                surahNumber={item.nomorSurah}
+                                verseData={item.verseData}
+                            />
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default JuzComp;
