e3ae81802ae0454fc5121e9b9f83805c276da757
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-09 21:14:06 +0700

    refactor code from props akses ke localstorage

--- src/component/JuzComp/DatajuzComp.jsx
+++ src/component/JuzComp/DatajuzComp.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const DatajuzComp = ({ surahNumber}) => {
-  const dataSurahLocalStorage = JSON.parse(localStorage.getItem('surah'));
-
-  if (!dataSurahLocalStorage) {
-    // Handle the case when dataSurahLocalStorage is not available
-    console.log('Data "surah" not found in localStorage.');
-    return null;
-  }
+const DatajuzComp = ({ surahNumber, judul, arti, asma, verseData}) => {
   
-
   return (
     <>
-      {Object.keys(surahNumber).map((key) => {
-        const nomorSurah = key;
-        const totalAyat = surahNumber[key];
-        const asmaData = dataSurahLocalStorage.find((surahItem) => surahItem.surah_number === nomorSurah);
-        const asma = asmaData ? asmaData.asma : '';
-        const judul = asmaData ? asmaData.name : '';
-        const arti = asmaData ? asmaData.arti : '';
-       
-       
-        return (
-          <Link to={`/${nomorSurah}`} key={nomorSurah}>
-            <div className="bg-gray-100 p-4 rounded-lg border -inset-1 mb-4">
+      <Link to={`/${surahNumber}/${verseData}`} key={surahNumber}>
+            <div className="bg-gray-100 p-4 rounded-lg border -inset-1 mb-4 mx-4">
               <div className="flex flex-row items-center justify-center">
                 <div className="flex items-center justify-center w-9 h-8 rounded-full bg-violet-500 text-white" alt="Avatar">
-                  <p className="text-sm">{nomorSurah}</p>
+                  <p className="text-sm">{surahNumber}</p>
                 </div>
                 <div className="w-full flex items-center justify-end text-sm ml-4">
                   <div className="w-[70%]">
@@ -41,19 +23,17 @@ const DatajuzComp = ({ surahNumber}) => {
                   <div className="w-[30%] text-right" style={{ fontFamily: 'LPMQ IsepMisbah' }}>
                     <div className="flex flex-col justify-end items-center">
                       <div>
-                        <p>{asma}</p>
+                        <p className=' text-lg'>{asma}</p>
                       </div>
                       <div>
-                        <p><span className='mx-2'>Ayat</span>{totalAyat}</p>
+                        <p><span className='mx-2'>Ayat</span>{verseData}</p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </Link>
-        );
-      })}
+      </Link>   
     </>
   );
 };
