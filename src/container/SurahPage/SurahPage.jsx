f47d07db22152a2d06d82f9eca316683fdbec389
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-09 21:06:59 +0700

    memindahkan tag html(storagesurah&surahpage)

--- src/container/SurahPage/SurahPage.jsx
+++ src/container/SurahPage/SurahPage.jsx
@@ -11,14 +11,12 @@ class SurahPage extends React.Component {
             <BarComp surah='/' juz='/juz' bookmark='/bookmark' />
 
               
-            <div className="min-h-screen bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-8 dark:bg-gray-800">
-              <div className=" grid grid-cols-1 gap-1 justify-center mx-4 md:grid md:grid-cols-2 md:gap-2 xl:grid xl:grid-cols-3 xl:gap-3">
-             
+            
                 <StorageSurah />
 
                    
-              </div>
-            </div>
+              {/* </div>
+            </div> */}
           </>
             
         );
