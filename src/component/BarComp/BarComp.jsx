d4319ac628aa58e51536be503d94dc1c623f779b
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-08-09 21:15:23 +0700

    add class dark bg & rubah ukuran warna bg

--- src/component/BarComp/BarComp.jsx
+++ src/component/BarComp/BarComp.jsx
@@ -1,10 +1,10 @@
 
 const BarComp = ({ surah, juz, bookmark }) => {
     return (
-        <div className="pt-[4rem] md:pt-[6rem]">
+        <div className="pt-[4rem] md:pt-[6rem] dark:bg-slate-800">
             <div className="mt-10 mb-4 flex justify-center items-center">
             
-                <div className="flex flex-wrap justify-center items-center w-[90%]  min-h-[2.5rem] md:w-[80%] xl:w-2/5 py-6 rounded-full bg-slate-900 text-white text-sm md:text-base sm:py-2">
+                <div className="flex flex-wrap justify-center items-center w-[90%]  min-h-[2.5rem] md:w-[80%] xl:w-2/5 py-6 rounded-full bg-slate-400 text-white text-sm md:text-base sm:py-2">
                     <div className=" w-1/3 md:w-1/3 xl:w-[10rem]">
                         <a
                         className="flex flex-wrap justify-center items-center"
